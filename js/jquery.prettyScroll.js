e5fac9957c2f7c489be19fb042a6c181747d19bd
Alexey <aleksejs.osovitnijs@trialine.lv>
2013-02-06 17:11:50 +0200

    adaptive bug fix

--- js/jquery.prettyScroll.js
+++ js/jquery.prettyScroll.js
@@ -18,18 +18,28 @@
 
         if(window.location.hash) {
             elem_to = $('[name="'+window.location.hash.replace('#', '')+'"]').not(options.exclude);
+
             if(elem_to.length>0) {
-                window.location.hash = '';
-                _prettyScroll(
-                    elem_to,
-                    false,
-                    function() {
-                        window.location.hash = elem_to.attr('name');
-                    }
-                );
+                orig_elem_to_name = elem_to.attr('name')
+
+                // save real anchor name
+                elem_to.data('orig_name', orig_elem_to_name );
+
+                // change name to avoid anchor standart jump
+                elem_to.attr('name', '_'+orig_elem_to_name );
+
+                $(window).load(function() {
+                    _prettyScroll(
+                        elem_to,
+                        false,
+                        function() {
+                            // change to real anchor name
+                            elem_to.attr('name', elem_to.data('orig_name') );
+                        }
+                    );
+                });
             }
         }
-
 
         $('a[href*=#]').not(options.exclude).click(function(e){
             elem_from = $(this);
@@ -38,7 +48,6 @@
             current_href = $(location).attr('href').split("#");
 
             if(current_href[0] == future_href[0]) {
-
                 _prettyScroll(
                     $('a[name='+future_href[1]+']'),
                     elem_from,
@@ -46,27 +55,25 @@
                  );
                 e.preventDefault();
             }
-
         });
 
 
+
         function _prettyScroll(elem_to, elem_from, callback) {
-            pixels = elem_to.offset().top;
+                pixels = elem_to.offset().top;
+                speed = options.speed;
 
-            speed = options.speed;
-            if(options.adaptive == true) {
-                if(elem_from == false) pixels_from = 0;
-                else pixels_from = elem_from.offset().top;
-                speed = Math.abs(pixels - pixels_from) / 4000 * speed;
-            }
+                if(pixels > $(document).height() - window.innerHeight) pixels = $(document).height() - window.innerHeight;
+
+                if(options.adaptive == true) {
+                    pixels_from = $('body').scrollTop();
+                    speed = parseInt(Math.abs(pixels - pixels_from) / 1000 * speed);
+                }
 
 
-            if(pixels > $(document).height() - window.innerHeight) pixels = $(document).height() - window.innerHeight;
-
-            $('body, html').animate({
-                scrollTop: pixels
-            }, speed, options.animation, callback);
-
+                $('html, body').animate({
+                    scrollTop: pixels
+                }, speed, options.animation, callback);
         }
         return this;
     };
